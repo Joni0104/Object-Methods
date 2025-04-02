226f7464bd987e0730f367dcd8365dbdb292bc4f
Joni0104 <joni99857622@mail.ru>
2025-04-02 20:14:47 +0300

    END homework

---
+++ src/Book.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Book {
+    private  String title;
+    private  Author author;
+    private  int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author + // Вызывается author.toString()
+                ", year=" + year +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+}
